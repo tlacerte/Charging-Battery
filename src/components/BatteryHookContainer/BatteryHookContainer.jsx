251f02a1a2cf31ec7433cee3a83e5567a89287ee
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-01 13:08:37 -0500

    use effect

--- src/components/BatteryHookContainer/BatteryHookContainer.jsx
+++ src/components/BatteryHookContainer/BatteryHookContainer.jsx
@@ -1,30 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import Battery from '../Battery/Battery'
+import React, {useState, useEffect} from "react";
+import Battery from "../Battery/Battery";
+import {register, unregister} from '../../utils/battery';
 
-function BatteryHookContainer(props){
-    const [batteryData, setBatteryData] = useState({
-        level: .55,
-        charging: true
-    });
+function BatteryHookContainer() {
+  const [batteryData, setBatteryData] = useState({
+    level: .55,
+    charging: true
+  });
 
-    useEffect(() => {
-        console.log('useEffect was called')
-    })
-    
-    return (
-        <>
-            <Battery 
-                level={batteryData.level} 
-                charging={batteryData.charging}
-            />
-            <button 
-                onClick={() => setBatteryData({
-                    level: batteryData.level + .01, 
-                    charging: false})}>
-                Update Level
-            </button>
-        </>
-    )
+  function updateBattery(data) {
+    setBatteryData(data);
+  }
+
+  useEffect(() => {
+    register(updateBattery);
+    return () => {
+      unregister(updateBattery);
+    };
+  }, []);
+
+  return (
+    <Battery level={batteryData.level} charging={batteryData.charging}/>
+  );
 }
 
-export default BatteryHookContainer
+export default BatteryHookContainer;
